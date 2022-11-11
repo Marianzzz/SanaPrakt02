ddb760eb9d764bbbe572fa6d724fabda527f17a6
Marian Tsyvinskyi <mariantop228@gmail.com>
2022-11-11 18:10:18 +0200

    Перший коміт на гілці text

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Sana Commerce Software Development Bootcamp");
+﻿using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("Слава Україні");
+Console.WriteLine("Слава ЗСУ");
+Console.WriteLine("Героям слава");
 Console.WriteLine("Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Sana Commerce Software Development Bootcamp");
